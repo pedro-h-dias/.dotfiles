8a8620ada68ab23ba1bfa0fc1285f1d15f219a1b
Pedro Dias <pdcmpa@gmail.com>
2019-06-19 22:13:03 -0300

    polybar working and launching at start

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -1,8 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-dotfiles=(".vimrc" ".gitconfig" ".bashrc")
-dir="${HOME}/.dotfiles"
+# Vim files
+ln -sf ~/.dotfiles/vim/vimrc 		~/.vimrc
 
-for dotfile in "${dotfiles[@]}";do
-	ln -sf "${HOME}/${dotfile}" "${dir}"
-done
+# Bash files
+ln -sf ~/.dotfiles/bash/bashrc	 	~/.bashrc
+ln -sf ~/.dotfiles/bash/bash_history	~/.bash_history 
+ln -sf ~/.dotfiles/bash/bash_profile	~/.bash_profile 
+ln -sf ~/.dotfiles/bash/bash_logout 	~/.bash_logout
+ln -sf ~/.dotfiles/bash/bash_aliases 	~/.bash_aliases
+
+# i3 files
+ln -sf ~/.dotfiles/i3/config 		~/.i3/config
