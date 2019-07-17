bff8086959b60ec559ecab24d92fd264dc7fc662
Pedro Dias <pdcmpa@gmail.com>
2019-07-17 09:34:36 -0300

    configs as of Jul 17, 2019

--- setup_monitors.sh
+++ setup_monitors.sh
@@ -3,12 +3,12 @@
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
 	if [ $m == 'DP1' ]
 	then
-		xrandr --output $m --mode 1920x1080
+		xrandr --output $m --auto
 	else
 		xrandr --output $m --auto 
 	fi
 done
 
-xrandr --output DP1 --right-of eDP1
+xrandr --output DP1 --left-of eDP1
 
 $HOME/.dotfiles/polybar/launch.sh 
