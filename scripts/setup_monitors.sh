e6fb43bebd61ba88573eb1418998a9b118f62419
Pedro Dias <pdcmpa@gmail.com>
2019-08-17 13:59:40 -0300

    fix: improves setup_monitors.sh, and adds folder to PATH

--- scripts/setup_monitors.sh
+++ scripts/setup_monitors.sh
@@ -7,8 +7,16 @@ for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
 	else
 		xrandr --output $m --auto 
 	fi
+done 
+
+while getopts "lrh" flag; do 
+  case "${flag}" in 
+    l) xrandr --output DP1 --left-of eDP1 
+	$HOME/.dotfiles/polybar/launch.sh ;;
+    r) xrandr --output DP1 --right-of eDP1
+	$HOME/.dotfiles/polybar/launch.sh ;;
+    h) echo "Usage: use -l for second monitor left-of main, or -r for it right-of main";;
+    *) echo "Unexpected option ${flag}. Use '-h' for help.";;
+  esac
 done
 
-xrandr --output DP1 --right-of eDP1
-
-$HOME/.dotfiles/polybar/launch.sh 
