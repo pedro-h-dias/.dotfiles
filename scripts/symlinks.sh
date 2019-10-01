3f159f675828c3bdcb7086195535b53af89a5ca7
Pedro Dias <pdcmpa@gmail.com>
2019-09-30 23:25:24 -0300

    refac: refac script to setup symlinks

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 # ssh files
-ln -sf ~/.dotfiles/ssh/config 			~/.ssh/config
+ln -sf ~/.dotfiles/ssh/config 			          ~/.ssh/config
 ln -sf ~/.dotfiles/systemd/ssh-agent.service 	~/.config/systemd/user/ssh-agent.service
+ln -sf ~/.dotfiles/systemd/load-keys.service  ~/.config/systemd/user/load-keys.service
 
 # Vim files
-ln -sf ~/.dotfiles/vim/vimrc			~/.vimrc
+ln -sf ~/.dotfiles/vim/vimrc			            ~/.vimrc
 
 # NeoVim files
-ln -sf ~/.dotfiles/nvim/init.vim		~/.config/nvim/init.vim
+ln -sf ~/.dotfiles/nvim/init.vim		          ~/.config/nvim/init.vim
 
 # Bash files
-ln -sf ~/.dotfiles/bash/bashrc	 		~/.bashrc
-ln -sf ~/.dotfiles/bash/bash_history		~/.bash_history 
-ln -sf ~/.dotfiles/bash/bash_profile		~/.bash_profile 
-ln -sf ~/.dotfiles/bash/bash_logout 		~/.bash_logout
-ln -sf ~/.dotfiles/bash/bash_aliases 		~/.bash_aliases
+ln -sf ~/.dotfiles/bash/bashrc	 		          ~/.bashrc
+ln -sf ~/.dotfiles/bash/bash_profile		      ~/.bash_profile 
+ln -sf ~/.dotfiles/bash/bash_logout 		      ~/.bash_logout
+ln -sf ~/.dotfiles/bash/bash_aliases 		      ~/.bash_aliases
 
 # i3 files
-ln -sf ~/.dotfiles/i3/config 			~/.i3/config
+ln -sf ~/.dotfiles/i3/config 			            ~/.i3/config
 
 # Others
-ln -sf ~/.dotfiles/.profile 			~/.profile
-ln -sf ~/.dotfiles/alacritty.yml		~/.config/alacritty/
-ln -sf ~/.dotfiles/gitconfig			~/.gitconfig
+ln -sf ~/.dotfiles/.profile 			            ~/.profile
+ln -sf ~/.dotfiles/alacritty.yml	            ~/.config/alacritty/
+ln -sf ~/.dotfiles/gitconfig			            ~/.gitconfig
+
