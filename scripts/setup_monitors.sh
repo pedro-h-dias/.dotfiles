6136765ebce4be17d5b9bcc7ee77e1ea63c7e565
Pedro Dias <pdcmpa@gmail.com>
2019-09-30 23:26:05 -0300

    refac: refac script to setup monitor to make it more general

--- scripts/setup_monitors.sh
+++ scripts/setup_monitors.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-	if [ $m == 'DP1' ]
+	if [ $m == 'eDP1' ]
 	then
 		xrandr --output $m --auto
 	else
 		xrandr --output $m --auto 
+    while getopts "lrah" flag; do 
+      case "${flag}" in 
+        l) xrandr --output $m --left-of eDP1 
+      $HOME/.dotfiles/polybar/launch.sh ;;
+        r) xrandr --output $m --right-of eDP1
+      $HOME/.dotfiles/polybar/launch.sh ;;
+        a) xrandr --output $m --above eDP1
+      $HOME/.dotfiles/polybar/launch.sh ;;
+        h) echo "Usage: use -l for second monitor left-of main, or -r for it right-of main";;
+        *) echo "Unexpected option ${flag}. Use '-h' for help.";;
+      esac
+    done
 	fi
 done 
-
-while getopts "lrah" flag; do 
-  case "${flag}" in 
-    l) xrandr --output DP1 --left-of eDP1 
-	$HOME/.dotfiles/polybar/launch.sh ;;
-    r) xrandr --output DP1 --right-of eDP1
-	$HOME/.dotfiles/polybar/launch.sh ;;
-    a) xrandr --output DP1 --above eDP1
-	$HOME/.dotfiles/polybar/launch.sh ;;
-    h) echo "Usage: use -l for second monitor left-of main, or -r for it right-of main";;
-    *) echo "Unexpected option ${flag}. Use '-h' for help.";;
-  esac
-done
 
 nitrogen --restore
