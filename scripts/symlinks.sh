6a5ce09997179c6033350d88a7b286e9384f1f2a
Pedro Dias <pdcmpa@gmail.com>
2020-07-03 18:47:11 -0300

    Initial commit for manjaro-gnome

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
 # ssh files
-ln -sf ~/.dotfiles/ssh/config 			          ~/.ssh/config
+ln -sf ~/.dotfiles/ssh/config 			~/.ssh/config
 ln -sf ~/.dotfiles/systemd/ssh-agent.service 	~/.config/systemd/user/ssh-agent.service
-ln -sf ~/.dotfiles/systemd/load-keys.service  ~/.config/systemd/user/load-keys.service
-
-# Vim files
-ln -sf ~/.dotfiles/vim/vimrc			            ~/.vimrc
+ln -sf ~/.dotfiles/systemd/load-keys.service  	~/.config/systemd/user/load-keys.service
 
 # NeoVim files
-ln -sf ~/.dotfiles/nvim/init.vim		          ~/.config/nvim/init.vim
-
-# Bash files
-ln -sf ~/.dotfiles/bash/bashrc	 		          ~/.bashrc
-ln -sf ~/.dotfiles/bash/bash_profile		      ~/.bash_profile 
-ln -sf ~/.dotfiles/bash/bash_logout 		      ~/.bash_logout
-ln -sf ~/.dotfiles/bash/bash_aliases 		      ~/.bash_aliases
-
-# i3 files
-ln -sf ~/.dotfiles/i3/config 			            ~/.i3/config
+ln -sf ~/.dotfiles/nvim/init.vim		~/.config/nvim/init.vim
 
 # Others
-ln -sf ~/.dotfiles/.profile 			            ~/.profile
-ln -sf ~/.dotfiles/alacritty.yml	            ~/.config/alacritty/
-ln -sf ~/.dotfiles/gitconfig			            ~/.gitconfig
+ln -sf ~/.dotfiles/.profile 			~/.profile
+ln -sf ~/.dotfiles/alacritty.yml	        ~/.config/alacritty/
+ln -sf ~/.dotfiles/gitconfig			~/.gitconfig
 
