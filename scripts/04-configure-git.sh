4191950aba16bfaceefa350752b16f6fac3b50f6
Pedro Dias <pdcmpa@gmail.com>
2020-07-11 15:55:35 -0300

    Add git config files

---
+++ scripts/04-configure-git.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# create link to git config
+ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+
+# clone repositories
+mkdir -p ~/ITA_Sec
+git clone --recursive git@github.com:pedro-h-dias/ITA.git ~/ITA
+git clone git@github.com:pedro-h-dias/wargames.git ~/ITA_Sec/wargames
+git clone git@github.com:pedro-h-dias/hackthebox.git ~/ITA_Sec/hackthebox
+git clone git@github.com:pedro-h-dias/ctf.git ~/ITA_Sec/ctf
+git clone git@github.com:ita-sec/images.git ~/ITA_Sec/images
