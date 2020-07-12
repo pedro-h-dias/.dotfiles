e88a3fa1bc8ec69221c07be6a5aef708d02345aa
Pedro Dias <phdias@protonmail.com>
2020-07-12 10:57:37 -0400

    Add tmux config file

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -3,4 +3,4 @@ ln -sf ~/.dotfiles/.profile 			~/.profile
 ln -sf ~/.dotfiles/.gitconfig			~/.gitconfig
 ln -sf ~/.dotfiles/ssh/config 			~/.ssh/config
 ln -sf ~/.dotfiles/nvim/init.vim		~/.config/nvim/init.vim
-
+ln -sf ~/.dotfiles/.tmux.conf 			~/.tmux.conf
