0c7229ad03ddecbde3921f987ab9a1addc1da97e
Pedro Dias <pdcmpa@gmail.com>
2020-07-15 01:56:18 -0300

    Add desktop entries for matlab and quartus, and fix dependencies

--- scripts/07-academic-env.sh
+++ scripts/07-academic-env.sh
@@ -7,8 +7,19 @@ yay -S --noconfirm --needed texlive-most
 
 # TODO: install latex plugins for vim
 
+# TODO: install inkscape extension for circuits
+
 # install dependencies for MATLAB
 yay -S --noconfirm --needed libselinux
+
+# install dependencies for ModelSim
+yay -S --noconfirm --needed lib32-libxft
+
+# link desktop entries for matlab and quartus
+ln -sf $HOME/.dotfiles/scripts/matlab.desktop $HOME/.local/share/applications
+ln -sf $HOME/.dotfiles/scripts/quartus.desktop $HOME/.local/share/applications
+ln -sf $HOME/.dotfiles/scripts/modelsim.desktop $HOME/.local/share/applications
+
 
 # install VirtualBox
 yay -S --noconfirm --needed virtualbox virtualbox-ext-oracle
