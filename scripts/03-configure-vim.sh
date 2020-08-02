ccfad6a0c7399c2a488033fdf102ea0740993235
Pedro Dias <pdcmpa@gmail.com>
2020-08-02 12:47:50 -0300

    Fix coc.nvim plugin installation

--- scripts/03-configure-vim.sh
+++ scripts/03-configure-vim.sh
@@ -11,6 +11,20 @@ ln -sf ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/installer.sh
 sh /tmp/installer.sh ~/.cache/dein
 
+# install nodejs for coc.nvim
+yay --needed --noconfirm -S nodejs yarn
+
+# for some reason, yarn compilation is failing with dein
+cd ~/.cache/dein/repos/github.com/neoclide/coc.nvim
+git clean -xfd
+yarn install --frozen-lockfile
+
 # install neovim plugins
 nvim +"call dein#install()" +qa
 nvim +"call dein#update()" +qa
+
+# install rust language server
+rustup component add rls rust-analysis rust-src
+
+# install coc-rls plugin
+nvim +":CocInstall coc-rls" +qa
