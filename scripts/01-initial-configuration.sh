a9df219532d15706538d36a06689c8c09b53752e
Pedro Dias <pdcmpa@gmail.com>
2022-11-22 16:03:11 -0300

    Add back scripts for git, ssh and initial config

--- scripts/01-initial-configuration.sh
+++ scripts/01-initial-configuration.sh
@@ -10,5 +10,10 @@ snap refresh
 # install essential tools
 sudo apt install build-essential -y
 
+# install programming tools
+sudo apt install curl -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo apt install nodejs -y
+
 # clean packages
 sudo apt autoremove -y
