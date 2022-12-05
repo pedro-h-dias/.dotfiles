4e89d529d8859498eb9fbfdd63c430aa10645d6d
Pedro Dias <pdcmpa@gmail.com>
2022-12-05 14:31:01 -0300

    Add scripts for work env

--- scripts/01-initial-configuration.sh
+++ scripts/01-initial-configuration.sh
@@ -8,12 +8,12 @@ sudo apt update && sudo apt upgrade -y
 snap refresh 
 
 # install essential tools
-sudo apt install build-essential -y
+sudo apt install build-essential cmake pkg-config libfontconfig-dev -y
 
 # install programming tools
 sudo apt install curl -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo apt install nodejs -y
+sudo apt install nodejs npm -y
 
 # clean packages
 sudo apt autoremove -y
---
+++ scripts/04-configure-work-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install google-cloud-cli
+sudo apt-get install -y apt-transport-https ca-certificates gnupg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-cli
+
+# Install pgadmin4
+curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
+echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list
+apt update
+sudo apt install pgadmin4
