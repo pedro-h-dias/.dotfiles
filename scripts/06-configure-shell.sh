43cfa222c3566f878acb09778119469c9be08fee
Pedro Dias <pdcmpa@gmail.com>
2023-02-12 17:09:01 -0300

    Add zsh configs

---
+++ scripts/06-configure-shell.sh
@@ -0,0 +1,14 @@
+# install zsh
+sudo apt install zsh -y
+chsh -s $(which zsh)
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# link .zshrc
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
