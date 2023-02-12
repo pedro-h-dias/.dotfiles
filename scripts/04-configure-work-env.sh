1237a0e4f633c67f4575c4fb290eebfc05f925bf
Pedro Dias <pdcmpa@gmail.com>
2023-02-12 17:08:46 -0300

    Improve installation scripts

--- scripts/04-configure-work-env.sh
+++ scripts/04-configure-work-env.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install coding environment
+sudo snap install code --classic
+
 # Install google-cloud-cli
 sudo apt-get install -y apt-transport-https ca-certificates gnupg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
@@ -8,6 +11,9 @@ sudo apt-get update && sudo apt-get install google-cloud-cli
 
 # Install pgadmin4
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
-echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list'
 apt update
 sudo apt install pgadmin4
+
+# Install dbeaver
+sudo snap install dbeaver-ce
--- scripts/05-configure-terminal.sh
+++ scripts/05-configure-terminal.sh
@@ -6,16 +6,29 @@ if ! which alacritty; then
   cargo install alacritty
 fi
 
-# install tmux
-if ! which tmux; then
-  sudo apt install -y tmux
-fi
-
 # create link to alacritty configs
 if [ ! -f ~/.config/alacritty/alacritty.yml ]; then
   mkdir -p ~/.config/alacritty
 fi
 ln -sf ~/.dotfiles/alacritty.yml ~/.config/alacritty/
+
+# set alacritty as default terminal
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator $HOME/.cargo/bin/alacritty 50
+sudo update-alternatives --config x-terminal-emulator
+
+# create desktop entry for alacritty
+git clone ttps://github.com/alacritty/alacritty.git
+pushd alacritty
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+popd
+rm -rf alacritty
+
+# install tmux
+if ! which tmux; then
+  sudo apt install -y tmux
+fi
 
 # create link to tmux configs
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
