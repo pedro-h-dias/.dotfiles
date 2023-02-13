32ad65daaa2f9c311393113b110899ba1a500f47
Pedro Dias <pdcmpa@gmail.com>
2023-02-13 10:21:40 -0300

    Add script with misc configurations

---
+++ scripts/08-configure-misc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install spotify
+sudo snap install spotify
+
+# install video player
+sudo snap install vlc
+
+# install image editor
+sudo snap install inkscape
+
+# install password manager
+sudo snap install bitwarden
